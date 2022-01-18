d0406d63f9766b7f315ae35f23651b85a4691276
Safia Abdale <safia.abdale@gmail.com>
2022-01-17 21:27:02 -0500

    Added preventDefault event handler on submitting tweet

--- react-version/src/components/TweetForm.js
+++ react-version/src/components/TweetForm.js
@@ -2,11 +2,12 @@ import React from 'react'; // optional
 
 function TweetForm(){
   const prompt = "What are you humming about?"
+  const preventDefault = (event) => {event.preventDefault()}
   return (
     <section className="newtweet">
-      <form method="post" action="/tweets" className="newtweet__form">
+      <form method="post" action="/tweets" className="newtweet__form" onClick={preventDefault}>
         <textarea className="form__textarea" name="text" placeholder={ prompt }></textarea>
-        <input type="submit" value="Tweet" className="form__input" />
+        <input type="submit" value="Tweet" className="form__input"/>
         <span className="form__counter">140</span>
       </form>
     </section>
