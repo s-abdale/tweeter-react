d6a8af950693aedab35905fb6dee86bcf3fc1afe
Safia Abdale <safia.abdale@gmail.com>
2022-01-17 20:19:27 -0500

    Broke down HTML & CSS components, recombined in React app

--- react-version/src/components/Navigation.js
+++ react-version/src/components/Navigation.js
@@ -2,7 +2,17 @@ import React from 'react'; // use this if version of React is older than v17. op
 
 function Navigation(){
   return (
-    <h1>I am the navigation component</h1>
+    <nav>
+      <span className="nav--text">tweeter</span>
+
+      <div className="btn btn--scroll">
+        <a href="#" className="btn__text btn--borderless" title="Compose Tweet">
+          <span className="btn--bold">Write</span> a new tweet
+          <br />
+          <i className="btn__icon fas fa-angle-double-down"></i>
+        </a>
+      </div>
+  </nav>
   );
 }
 
--- react-version/src/components/Profile.js
+++ react-version/src/components/Profile.js
@@ -2,7 +2,15 @@ import React from 'react'; // optional
 
 function Profile(){
   return (
-    <h1>I am the profile component</h1>
+    <aside>
+      <div className="profile">
+        <img className="profile__image" src="./profile-hex.png" />
+      </div>
+      <br />
+      <div className="profile__name">
+        <h2><span className="profile--bold">Amy</span> Mansell</h2>
+      </div>
+    </aside>
   );
 }
 
