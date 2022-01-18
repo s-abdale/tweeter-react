fae85f3d5ab89d2ed6c49890a4ecdaa618abe087
Safia Abdale <safia.abdale@gmail.com>
2022-01-17 19:52:52 -0500

    Created files for the various page components in react

---
+++ react-version/src/components/Navigation.js
@@ -0,0 +1,9 @@
+import React from 'react'; // use this if version of React is older than v17. optional for this project since my react version is newer
+
+function Navigation(){
+  return (
+    <h1>I am the navigation component</h1>
+  );
+}
+
+export default Navigation;
