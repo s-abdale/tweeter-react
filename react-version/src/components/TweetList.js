30b9dffb2c5a2361a42cd11b77199adef7d5a074
Safia Abdale <safia.abdale@gmail.com>
2022-01-17 21:01:13 -0500

    Separated individual tweets from the TweetList

--- react-version/src/components/TweetList.js
+++ react-version/src/components/TweetList.js
@@ -1,29 +1,16 @@
 import React from 'react'; // optional
+import Tweet from './Tweet'
+
 
 function TweetList(){
+
   return (
     <section className="tweets">
-      <article className="tweet">
-        <header className="tweet--header">
-          <img className="tweet--avatar" src="https://i.imgur.com/73hZDYK.png" />
-          <h2 className="tweet--name">Newton</h2>
-          <small className="tweet--handle">@SirIsaac</small>
-        </header>
+      <Tweet />
+      <Tweet />
 
-        <div className="tweet--body">
-          <p>If I have seen further it is by standing on the shoulders of giants</p>
-        </div>
 
-        <footer className="tweet--footer">
-          <small className="footer--age">10 days ago<small>
-              <span className="footer--actions">
-                <a href="#"><i className="fa fa-flag"></i></a>
-                <a href="#"><i className="fa fa-retweet"></i></a>
-                <a href="#"><i className="fa fa-heart"></i></a>'
-              </span>
-            </small></small></footer>
-      </article>
-      <article className="tweet">
+      {/* <article className="tweet">
         <header className="tweet--header">
           <img className="tweet--avatar" src="https://i.imgur.com/nlhLi3I.png" />
           <h2 className="tweet--name">Descartes</h2>
@@ -42,7 +29,7 @@ function TweetList(){
                 <a href="#"><i className="fa fa-heart"></i></a>'
               </span>
             </small></small></footer>
-      </article>
+      </article> */}
 
     </section>
   );
